20a073754e89e912134621f61c91c8e0f0837a50
MikelGV <mikelgaldosv@gmail.com>
2026-03-10 11:12:05 +0100

    FEATURE: ACKJob function addded
    Also added a processjobs helper function so that the consumeJobs
    function is tighter

--- internal/broker/consumer.go
+++ internal/broker/consumer.go
@@ -16,54 +16,94 @@ import (
 func (rds *RedisStore) ConsumeJobs(ctx context.Context, streamName, groupName, consumerName string, dispatcher *dispatcher.Dispatcher) error {
 	for {
 
-		res, err := rds.Conn.XReadGroup(ctx, &redis.XReadGroupArgs{
-			Group:    groupName,
-			Consumer: consumerName,
-			Streams:  []string{streamName, ">"},
-			Block:    0,
-			Count:    50,
-		}).Result()
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			res, err := rds.Conn.XReadGroup(ctx, &redis.XReadGroupArgs{
+				Group:    groupName,
+				Consumer: consumerName,
+				Streams:  []string{streamName, ">"},
+				Block:    0,
+				Count:    50,
+			}).Result()
 
-		if err != nil {
-			if err == context.Canceled {
-				return err
+			if err != nil {
+				if err == context.Canceled {
+					return err
+				}
+				fmt.Printf("Something went wrong when trying to read the job: %s /n", err)
+
+				continue
 			}
-			fmt.Printf("Something went wrong when trying to read the job: %s /n", err)
+			for _, streams := range res {
+				rds.ProcessJobs(ctx, streams.Messages, dispatcher, streamName, groupName)
+			}
 
+		}
+
+	}
+}
+
+func (rds *RedisStore) ProcessJobs(ctx context.Context, Messages []redis.XMessage, dispatcher *dispatcher.Dispatcher, streamName, groupName string) {
+	for _, msg := range Messages {
+
+		taskreq, err := task.FromFields(msg.Values)
+		if err != nil {
+			fmt.Printf("Something went wrong: %s /n", err)
 			continue
 		}
 
-		for _, streams := range res {
-			for _, msg := range streams.Messages {
-
-				taskreq, err := task.FromFields(msg.Values)
-				if err != nil {
-					fmt.Printf("Something went wrong: %s /n", err)
-					continue
-				}
-
-				job := &task.Job{
-					ID:          msg.ID,
-					TYPE:        taskreq.Type,
-					PAYLOAD:     taskreq.Payload,
-					ATTEMPT:     0,
-					MAX_RETRIES: 3,
-				}
-
-				err = dispatcher.DispatchTask(ctx, job)
-				if err != nil {
-					fmt.Printf("Something went wrong trying to dispatch task: %s /n", err)
-					continue
-				}
-
-			}
+		job := &task.Job{
+			ID:          msg.ID,
+			TYPE:        taskreq.Type,
+			PAYLOAD:     taskreq.Payload,
+			ATTEMPT:     0,
+			MAX_RETRIES: 3,
 		}
+
+		err = dispatcher.DispatchTask(ctx, job)
+		if err != nil {
+			fmt.Printf("Something went wrong trying to dispatch task: %s /n", err)
+			continue
+		}
+
+		_, err = rds.AckJob(ctx, streamName, groupName, msg.ID)
+		if err != nil {
+			fmt.Printf("xAck failed after success id=%s, err=%v/n", msg.ID, err)
+		}
+
 	}
+
+}
+
+/**
+* Acknwoledge job
+**/
+func (rds *RedisStore) AckJob(ctx context.Context, streamName, groupName, msgId string) (int64, error) {
+
+	if msgId == "" {
+		return 0, fmt.Errorf("msgId can not be empty")
+	}
+
+	acked, err := rds.Conn.XAck(ctx, streamName, groupName, msgId).Result()
+	if err != nil {
+		return 0, fmt.Errorf("xAck %s %s %s failed: %w\n", streamName, groupName, msgId, err)
+	}
+
+	return acked, nil
+}
+
+/**
+* Checks what consumers are active
+**/
+func (rds *RedisStore) CheckLiveGroups(msgId string) error {
+	return nil
 }
 
 // This is were we claim jobs that haven't been acknwoledged by the ack
 
-func (rds *RedisStore) ClaimPendingJobs(ctx context.Context, streamName, consumerName string) (string, error) {
+func (rds *RedisStore) ClaimFailedJobs(ctx context.Context, streamName, consumerName string) (string, error) {
 	return "", nil
 }
 
