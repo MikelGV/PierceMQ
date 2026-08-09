be2c1aef696286d777e4f9a776681d96c1e13d8c
MikelGV <mikelgaldosv@gmail.com>
2026-08-09 14:26:59 +0200

    WIP: have to keep thinking of how the workers get started
    Also have to think about how they get paired in pools and how to handle
    the pools.

--- tests/worker/worker_test.go
+++ tests/worker/worker_test.go
@@ -6,10 +6,15 @@ import (
 	"github.com/MikelGV/PierceMQ/internal/worker"
 )
 
+/**
+* I need to rewrite this tests to because I might change things in the create
+* function
+**/
 func TestNewWorker(t *testing.T) {
 	t.Run("Worker is created successfully", func(t *testing.T) {
 		var w *worker.Worker
 
+		// I have to think for a way to increment the workers or increament the active pools
 		woker := w.NewWorker(1, make(chan *worker.Worker))
 
 		t.Log(woker)
@@ -18,7 +23,7 @@ func TestNewWorker(t *testing.T) {
 	t.Skip("Worker creation fails due to bad/missing connection", func(t *testing.T) {})
 }
 
-func TestRun_Pool(t *testing.T) {
+func TestRun(t *testing.T) {
 	t.Skip("Worker pool is created successfully", func(t *testing.T) {})
 
 	t.Skip("Worker creation fails due to bad/missing connection", func(t *testing.T) {})
