d5c2206a312f022b163a1b2a3c33610f9a46bd78
MikelGV <mikelgaldosv@gmail.com>
2026-08-17 07:22:38 +0200

    WIP: Working on processing the jobs and the Run(main loop) function

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -1,6 +1,12 @@
 package worker
 
-import "github.com/MikelGV/PierceMQ/internal/task"
+import (
+	"context"
+	"fmt"
+
+	"github.com/MikelGV/PierceMQ/internal/task"
+	"github.com/redis/go-redis/v9"
+)
 
 type Worker struct {
 	ID         int
@@ -11,17 +17,34 @@ type Worker struct {
 /**
 	* Create a new worker pool
 **/
-func (*Worker) NewWorker(id int, workerpool chan *Worker) *Worker {
+func (w *Worker) NewWorker(id int, workerpool chan *Worker, conn *redis.Client) (*Worker, error) {
+	if conn == nil {
+		return nil, fmt.Errorf("worker %d: nil redis connection", id)
+	}
+
+	if err := conn.Ping(context.Background()).Err(); err != nil {
+		return nil, fmt.Errorf("worker %d: lost connection to redis: %w", id, err)
+	}
+
 	return &Worker{
 		ID:         id,
 		JobChannel: make(chan *task.Job),
 		Worker:     workerpool,
-	}
+	}, nil
 }
 
 /**
 	* Runs the worker main loop
+	* Here we run the processJobs the error handling,etc, ...
 **/
-func (*Worker) Run() error {
+func (w *Worker) Run() error {
 	return nil
+}
+
+/**
+* This function process all jobs differentiating each type of job and doing what
+* it requires to complete them
+**/
+func (w *Worker) processJobs() {
+
 }
